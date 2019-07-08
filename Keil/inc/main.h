757f203f3f3d311607ab1d96ac03761ef0120ec6
Evan Reeves <evanreeves12@gmail.com>
2019-07-07 18:15:22 -0700

    initial commit

---
+++ Keil/inc/main.h
@@ -0,0 +1,21 @@
+/*
+* @author Evan Reeves
+*/
+
+#include "stm32f4xx_hal.h"
+
+typedef struct {
+  uint16_t hours;
+  uint16_t minutes;
+  uint16_t seconds;
+}time;
+
+void GPIO_Init(void);
+void inc_seconds(void);
+void inc_minutes(void);
+void inc_hours(void);
+void update_display(void);
+void HAL_IncTick(void);
+static void SystemClock_Config(void);
+void ConfigureADC(void);
+uint16_t int2bcd(uint16_t);
